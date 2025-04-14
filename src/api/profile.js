e4b5a5c7ce59a4da485ab6ba1816b72845cd4e1f
Shahadeissa8 <shahadeissa98@gmail.com>
2025-04-14 11:28:47 +0300

    Need to fix the userId

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,13 +1,16 @@
 import instance from "."; // Import axios instance
 import { getToken } from "./storage"; // Import getToken to retrieve the token
 
-const getUserProfile = async (id) => {
+const getUserProfile = async () => {
   try {
     const token = await getToken(); // Retrieve the token from storage
     if (!token) {
       throw new Error("Token is missing. Please log in again.");
     }
-
+    // TODO : Replace with the actual user ID from your application logic
+    const userId = "f5ff8593-78cc-455d-bcc3-083e6788f4ff";
+    console.log("Fetching user profile with ID:", id);
+    console.log("Using token:", token);
     const response = await instance.get(`/ApplicationUser/${id}`, {
       headers: {
         Authorization: `Bearer ${token}`, // Include the token in the Authorization header
@@ -15,7 +18,7 @@ const getUserProfile = async (id) => {
     });
     return response.data;
   } catch (error) {
-    console.error("Error fetching user profile:", error);
+    console.error("Error fetching user profile:", error.message);
     throw new Error("Failed to fetch user profile.");
   }
 };
@@ -40,23 +43,10 @@ const changePassword = async (currentPassword, newPassword) => {
   } catch (error) {
     console.error("Error changing password:", error);
     throw new Error(
-      error.response?.data?.title || "Failed to change password. Please try again."
+      error.response?.data?.title ||
+        "Failed to change password. Please try again."
     );
   }
 };
 
 export { getUserProfile, changePassword };
-
-
-
-
-
-
-
-
-
-
-
-
-
-

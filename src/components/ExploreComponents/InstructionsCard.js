b890b215f05ca57697f5cbe08ddaff86b8da9e54
A-AlHirz <abdherz@hotmail.com>
2025-05-05 10:04:20 +0300

    InstructionsCard added pic3

--- src/components/ExploreComponents/InstructionsCard.js
+++ src/components/ExploreComponents/InstructionsCard.js
@@ -2,47 +2,49 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Animated } from "react-native";
 
 const ImageLoader = () => {
-  // Array of images you want to display; add as many as you want here
   const images = [
     require("../../../assets/pic1.png"),
     require("../../../assets/pic2.png"),
-    // Add more images as required
+    require("../../../assets/pic3.png"),
   ];
 
-  // Set initial opacity values for all images
   const [opacities, setOpacities] = useState(
-    images.map((_, index) => new Animated.Value(index === 0 ? 1 : 0)) // Set the first image to opacity 1
+    images.map((_, index) => new Animated.Value(index === 0 ? 1 : 0))
   );
 
-  const [currentIndex, setCurrentIndex] = useState(0); // Index to track current image
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Function to transition between images
   const switchImage = () => {
-    const nextIndex = (currentIndex + 1) % images.length; // Cycle through the images
+    const nextIndex = (currentIndex + 1) % images.length;
 
-    // Fade out the current image and fade in the next image at the same time
     Animated.parallel([
       Animated.timing(opacities[currentIndex], {
-        toValue: 0, // Fade out the current image
+        toValue: 0,
         duration: 2000,
         useNativeDriver: true,
       }),
       Animated.timing(opacities[nextIndex], {
-        toValue: 1, // Fade in the next image
+        toValue: 1,
         duration: 2000,
         useNativeDriver: true,
       }),
     ]).start(() => {
-      setCurrentIndex(nextIndex); // Update the current image index
+      setCurrentIndex(nextIndex);
     });
   };
 
   useEffect(() => {
-    // Set the interval to switch images every 3 seconds
-    const interval = setInterval(switchImage, 3000);
+    let isMounted = true;
 
-    // Cleanup on component unmount
-    return () => clearInterval(interval);
+    const interval = setInterval(() => {
+      if (isMounted) switchImage();
+    }, 3000);
+
+    return () => {
+      isMounted = false;
+      clearInterval(interval);
+      opacities.forEach((opacity) => opacity.stopAnimation());
+    };
   }, [currentIndex]);
 
   return (
